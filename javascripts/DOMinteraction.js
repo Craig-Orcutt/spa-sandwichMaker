ceb320a7d04394b262023159bcb2f3d052ccfe5e
Craig Orcutt <craigaorcutt@gmail.com>
2017-12-19 15:39:13 -0600

    added stuff

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -7,18 +7,28 @@ let meat = document.getElementById('meat');
 let cheese = document.getElementById('cheese');
 let condiments = document.getElementById('condiments');
 let veggies = document.getElementById('veggies');
+let output = document.getElementById('output');
 // required the sandwich maker module into dom module
 let sandwichMaker = require("./sandwichMaker");
 
 
 // EL for submit button
 submit.addEventListener('click', function(){
-    console.log('button clicked');
+    output.innerHTML = `${sandwichMaker.getTotal()} ${sandwichOutput(sandwichMaker.getSandwich())}`;
 });
 
 menu.addEventListener('change', function(){
     sandwichMaker.addIngredient(event.target.closest('div').id, event.target.value);
 });
 
-
+let sandwichOutput = function (sandwichObject) {
+    let sandwichString = "";
+    for(let ingredient in sandwichObject){
+        console.log(sandwichObject[ingredient]);
+        for(let i = 0; i < sandwichObject[ingredient].length; i++){
+            sandwichString += `${sandwichObject[ingredient][i]} `;
+        }
+    }
+    return sandwichString;
+};
 
--- javascripts/sandwichMaker.js
+++ javascripts/sandwichMaker.js
@@ -2,9 +2,25 @@
 require("./DOMinteraction");
 let bread = require('./bread');
 let total = 0;
+let sandwich = {
+    "bread" : [],
+    "meat" : [],
+    "cheese" : [],
+    "condiments" : [],
+    "veggies" : []
+};
 
 module.exports.addIngredient = function(id, value){
+    sandwich[id].push(value);
+    console.log(sandwich);
     total += bread.addBread(value);
     console.log(total);
 };
 
+module.exports.getTotal = function(){
+    return total;
+};
+
+module.exports.getSandwich = function(){
+    return sandwich;
+};
