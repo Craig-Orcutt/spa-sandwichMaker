e4d53959dba61f8b4ffcf610def3bd67f2910c25
Craig Orcutt <craigaorcutt@gmail.com>
2017-12-19 14:18:20 -0600

    created function to add the price of bread

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -1,4 +1,5 @@
 'use strict';
+
 let menu = document.getElementById('menu');
 let submit = document.getElementById("submit");
 let bread  = document.getElementById('bread');
@@ -6,6 +7,8 @@ let meat = document.getElementById('meat');
 let cheese = document.getElementById('cheese');
 let condiments = document.getElementById('condiments');
 let veggies = document.getElementById('veggies');
+// required the sandwich maker module into dom module
+let sandwichMaker = require("./sandwichMaker");
 
 
 // EL for submit button
@@ -14,7 +17,8 @@ submit.addEventListener('click', function(){
 });
 
 menu.addEventListener('change', function(){
-    console.log(event.target.value);
-    console.log(event.target.closest('div').id);
+    sandwichMaker.addIngredient(event.target.closest('div').id, event.target.value);
 });
+
+
 
--- javascripts/sandwichMaker.js
+++ javascripts/sandwichMaker.js
@@ -1,6 +1,10 @@
 'use strict';
-
-
-
-
 require("./DOMinteraction");
+let bread = require('./bread');
+let total = 0;
+
+module.exports.addIngredient = function(id, value){
+    total += bread.addBread(value);
+    console.log(total);
+};
+
